8e969448fb226abd721291e691d4b574e95ac30b
jalkanen <jalkanen@340ca205-3d50-0410-8bca-96afde431eef>
2008-08-01 18:37:09 +0000

    Minor tweaks and indexes.

--- etc/setup-jdbcprovider.sql
+++ etc/setup-jdbcprovider.sql
@@ -2,13 +2,13 @@ drop table propertyvalues if exists;
 drop table nodes if exists;
 drop table workspaces if exists;
 
-create table workspaces (
+create memory table workspaces (
 	id 		integer 		identity primary key,
 	name	varchar(100) 	not null
 	
 );
 
-create unique index idx_workspaces_name on workspaces(name);
+-- create unique index idx_workspaces_name on workspaces(name);
 
 create table nodes (
 	id		bigint 			identity primary key,
@@ -23,16 +23,19 @@ create table nodes (
 );
 
 create index idx_nodes_path on nodes(path);
+create index idx_nodes_id   on nodes(id,path);
 
 create table propertyvalues (
 	id		bigint			identity primary key,
 	parent	bigint 			not null,
 	name	varchar(64)		not null,
 	type	tinyint			not null,
+	multi   boolean         not null,
 	len     bigint,
 	propval	binary,
 	
-	foreign key (parent)	references nodes (id)
+	foreign key (parent)	references nodes (id),
+	constraint uniq_prop    unique(parent,name)
 );
 
 create index idx_propertyvalues_name on propertyvalues(name);
